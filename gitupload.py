b0de7d796ef3b8e49694d0cff5df39b81e2c408d
wittfan <wittfan@qq.com>
2019-06-07 15:00:08 +0800

    20190607

---
+++ gitupload.py
@@ -0,0 +1,13 @@
+import sys,os
+import gevent
+os.system("git status")
+os.system("git add *")
+os.system("git commit -m 'add files'")
+os.system("git push")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+
