0a628591e837bd3e4948256fe5c28528a5ea3b0d
wittfan <wittfan@qq.com>
2019-06-08 11:44:19 +0800

    Sat Jun  8 11:44:19 2019

--- gitupload.py
+++ gitupload.py
@@ -2,7 +2,7 @@ import sys,os
 import time
 os.system("git status")
 os.system("git add *")
-now=time.ctime
+now=time.ctime()
 os.system("git commit -m '%s'" %now)
 os.system("git push")
 os.system("")
